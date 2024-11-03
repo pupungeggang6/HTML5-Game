5337debf255742640e6f05acb53a279f53122846
pupungeggang6 <pupungeggang6@gmail.com>
2024-11-04 03:46:16 +0900

    Start

--- Defense/Game/Script/Main.js
+++ Defense/Game/Script/Main.js
@@ -3,11 +3,51 @@ window.onerror = errorHandle
 window.oncontextmenu = rightClick
 
 function main() {
-    
+    canvas = document.getElementById('Screen')
+    context = canvas.getContext('2d')
+
+    canvas.addEventListener('mouseup', mouseUp, false)
+
+    gameFrameCurrent = Date.now()
+    gameFramePrevious = Date.now() - 16
+    gameLoop = requestAnimationFrame(loop)
 }
 
-function errorHandle() {
+function loop() {
+    gameFramePrevious = gameFrameCurrent
+    gameFrameCurrent = Date.now()
+    delta = gameFrameCurrent - gameFramePrevious
 
+    if (scene === 'Title') {
+        loopTitle()
+    } else if (scene === 'Ready') {
+        loopReady()
+    } else if (scene === 'Game') {
+        loopGame()
+    }
+
+    gameLoop = requestAnimationFrame(loop)
+}
+
+function mouseUp(event) {
+    let targetRect = canvas.getBoundingClientRect()
+    let x = (event.clientX - targetRect.left) / targetRect.width * canvas.width
+    let y = (event.clientY - targetRect.top) / targetRect.height * canvas.height
+    let button = event.button
+
+    if (scene === 'Title') {
+        mouseUpTitle(x, y, button)
+    } else if (scene === 'Ready') {
+        mouseUpReady(x, y, button)
+    } else if (scene === 'Game') {
+        mouseUpGame(x, y, button)
+    }
+}
+
+function errorHandle(err, url, line, col, obj) {
+    if (obj != null) {
+        cancelAnimationFrame(gameLoop)
+    }
 }
 
 function rightClick() {
---
+++ Defense/Game/Script/SceneTitle.js
@@ -0,0 +1,24 @@
+function loopTitle() {
+    displayTitle()
+}
+
+function displayTitle() {
+    drawSceneInit()
+
+    context.fillText('Defense', UI.title.textTitle[0], UI.title.textTitle[1])
+    context.strokeRect(UI.title.buttonStart[0], UI.title.buttonStart[1], UI.title.buttonStart[2], UI.title.buttonStart[3])
+    context.fillText('Game Start', UI.title.textStart[0], UI.title.textStart[1])
+}
+
+function mouseUpTitle(x, y, button) {
+    if (button === 0) {
+        if (menu === false) {
+            if (state === '') {
+                if (pointInsideRectArray(x, y, UI.title.buttonStart)) {
+                    scene = 'Game'
+                    state = ''
+                }
+            }
+        }
+    }
+}
